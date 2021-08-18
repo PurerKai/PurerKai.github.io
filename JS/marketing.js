aacd04fb730a3e18cfe681cae1b5550ff4e60dcf
PurerKai <kaikai15191519@gmail.com>
2021-08-18 19:49:30 +0800

    修改優缺點

--- JS/marketing.js
+++ JS/marketing.js
@@ -3,5 +3,5 @@ $(".d456").click(function (e) {
     $(".d456").removeClass("know")
     $(this).addClass("know")
     $('.tab').addClass("fade")
-    $(`${$((this)).data('look')}`).removeClass("fade")
+    $(`${$(this).data('look')}`).removeClass("fade")
 });
