a4aa6284d8bfa968b7d8fddb8b8b66b801ed9d41
PurerKai <kaikai15191519@gmail.com>
2022-05-04 03:31:20 +0800

    AA

--- javascriptexericse/apple/index.js
+++ javascriptexericse/apple/index.js
@@ -3,113 +3,77 @@ var globel_item = {
     storage: "0"
 }
 window.onload = function () {
-
     getJson("./data.json");
     // https://purerkai.github.io/javascriptexericse/apple/ipadair.json    
 }
 function createTitle(data) {
-    var ul = $q(".nav-pills");
-    for (const i in Object.keys(data)) {
-        var id = Object.keys(data)[i];
-        let cloneNav = $q('#pill-links').content.cloneNode(true);
+    var pills = $q("#pills-tab1");
+    for (const type of Object.keys(data)) {
+        let cloneNav = $q('#temp-pills-tab1').content.cloneNode(true);
         let btn = cloneNav.querySelector("button");
-        btn.setAttribute("data-bs-toggle", "pill");
-        btn.innerHTML = id;
-        if (i == 0) {
-            btn.classList.add("active");
-        }
-        ul.append(cloneNav);
-        btn.addEventListener("click", (e) => {
-            $q(`#price-span`).innerHTML = "";
-            createContent(data, e.target.innerHTML);
-            globel_item.storage= "0";
-            globel_item.network= "0";
+        btn.setAttribute("data-type", type);
+        btn.innerHTML = type;
+        btn.addEventListener("click", () => {
+            let Main = $q(".item-content");
+            Main.innerHTML = "";
+            $q(`#pills-tab2`).innerHTML = "";
+            for (const item in data[type]) {
+                let cloneNav2 = $q('#temp-pills-tab1').content.cloneNode(true);
+                let btn2 = cloneNav2.querySelector("button");
+                btn2.setAttribute("data-item", item);
+                btn2.innerHTML = item;
+                btn2.addEventListener("click", () => {
+                    createContent(data[type], item)
+                })
+                $q(`#pills-tab2`).append(cloneNav2)
+            }
         })
+        pills.append(cloneNav);
     }
 }
 function createContent(data, id) {
+    setBanner("./img/all-" + id + ".jpg")
+    let Main = $q(".item-content");
+    Main.innerHTML = "";
+    for (const x of Object.keys(data[id])) {
+        let clist = $q('#temp-list').content.cloneNode(true).querySelector("div");
+        clist.setAttribute("id", `${id}-${x}-list`)
+        for (const attr of Object.keys(data[id][x])) {
+            let citem = $q('#temp-list-item').content.cloneNode(true);
+            citem.addEventListener("click", () => {
+                banner.setAttribute("src", "./img/" + id + "-" + color + ".png");
+            })
+            citem.querySelector(".col").setAttribute("id", `${attr}`);
+            var img = citem.querySelector("img");
+            img.setAttribute("src", "./img/color-" + attr + ".jfif");
+            img.setAttribute("alt", `color-${attr}`);
+            var p = citem.querySelector("p");
+            p.innerHTML = data[id][x][attr];
+            clist.appendChild(citem);
+        }
+        Main.append(clist);
+        if (Object.keys(data[id]).indexOf("size")!=-1)
+            clickColorImg(Main,id,x,`./img/${id}-11`);
+        else
+            clickColorImg(Main,id,x,`./img/${id}`);
+    }
+}
+function clickColorImg(m,id,x,url) {
+    let ul = m.querySelectorAll(`#${id}-${x}-list>div`);
+    for (const li of ul) {
+        li.addEventListener("click", e => {
+            for (const i of ul) {
+                i.querySelector("div").classList.remove("border-primary");
+            }
+            li.querySelector("div").classList.add("border-primary");
+            if (x == "color")
+                setBanner(url+`-${li.id}.png`);
+        })
+    }
+}
+function setBanner(url) {
     let banner = $q("#banner-img");
-    banner.setAttribute("src", "./img/" + id + "-all.jpg");
-
-    let color_list = $q("#color-list");
-    color_list.innerHTML = "";
-    for (const color of Object.keys(data[id].color)) {
-        let cloneContent = $q('#temp-color').content.cloneNode(true);
-        var box = cloneContent.querySelector("li");
-        box.addEventListener("click", () => {
-            banner.setAttribute("src", "./img/"+id+"-"+ color + ".png");
-        })
-        box.setAttribute("id", `color-${color}`);
-        var img = cloneContent.querySelector("img");
-        img.setAttribute("src", "./img/" + color + ".jfif");
-        img.setAttribute("alt", color);
-        var p = cloneContent.querySelector("p");
-        p.innerHTML = data[id].color[color];
-        color_list.append(cloneContent);
-    }
-    let color_li = color_list.querySelectorAll("li");
-    for (const node of color_li) {
-        node.addEventListener("click", e => {
-            var id = node.id;
-            for (const n of color_li) {
-                n.classList.remove("border")
-            }
-            $q(`#${id}`).classList.add("border");
-        })
-    }
-
-    let storage_list = $q("#storage-list");
-    storage_list.innerHTML = "";
-    for (const storage of Object.keys(data[id].storage)) {
-        let cloneContent = $q('#temp-storage').content.cloneNode(true);
-        var box = cloneContent.querySelector("li");
-        box.setAttribute("id", `storage-${storage}`);
-        box.addEventListener("click", () => {
-            globel_item.storage = data[id].storage[storage]
-            showPrice();
-        })
-        var p = cloneContent.querySelector("p");
-        p.innerHTML = storage;
-        storage_list.append(cloneContent);
-    }
-    let storage_li = storage_list.querySelectorAll("li");
-    for (const node of storage_li) {
-        node.addEventListener("click", e => {
-            var id = node.id;
-            for (const n of storage_li) {
-                n.classList.remove("border")
-            }
-            $q(`#${id}`).classList.add("border");
-        })
-    }
-
-    let network_list = $q("#network-list");
-    network_list.innerHTML = "";
-    if(data[id].network != undefined){
-        for (const network of Object.keys(data[id].network)) {
-            let cloneContent = $q('#temp-network').content.cloneNode(true);
-            var box = cloneContent.querySelector("li");
-            box.setAttribute("id", `network-${network}`);
-            box.addEventListener("click", () => {
-                globel_item.network = data[id].network[network]
-                showPrice();
-            })
-            var p = cloneContent.querySelector("p");
-            p.innerHTML = network;
-            network_list.append(cloneContent);
-        }
-        let network_li = network_list.querySelectorAll("li");
-        for (const node of network_li) {
-            node.addEventListener("click", e => {
-                var id = node.id;
-                for (const n of network_li) {
-                    n.classList.remove("border")
-                }
-                $q(`#${id}`).classList.add("border");
-            })
-        }
-    }
-
+    banner.setAttribute("src", url);
 }
 function getJson(url) {
     fetch(url)
